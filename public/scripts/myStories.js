54f86691e586dfc605daf627c0b30aa16fba8f0f
Joseph Cheverton-Wynne <jchevertonwynne@gmail.com>
2020-05-29 15:55:35 +0100

    changes

--- public/scripts/myStories.js
+++ public/scripts/myStories.js
@@ -1,13 +1,26 @@
-const allLoaded = async () => {
-    loadMyStories().then(displayStories);
+const getMyStories = async () => {
     fetch('/stories/myStories')
         .then(status)
         .then(r => r.json())
         .then(async allStories => {
             await storeMyStories(allStories);
-            // const loadedStories = await Promise.all(allStories.map( element => getImages(element).then(saveMyStory)));
             loadMyStories().then(displayStories);
         }).catch(console.log());
+}
+
+const allLoaded = async () => {
+    loadMyStories().then(displayStories);
+    getMyStories();
+}
+
+try {
+    const socket = io();
+    socket.on("NewStoryPost", () => {
+        getMyStories();
+    });
+}
+catch (err) {
+    console.log(err);
 }
 
 window.addEventListener('load', allLoaded, false);
